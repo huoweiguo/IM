b2027094c2eabd4c9ba785b7c97e91e9e3928592
lihy <479140237@qq.com>
2025-08-08 12:03:09 +0800

    fix: 修复登录token处理并添加登录过期检查
    更新登录逻辑以正确处理apptoken和servicetoken
    在Layout组件中添加登录过期检查及登出功能
    统一请求头中的token为apptoken

--- src/qzui/components/Layout.vue
+++ src/qzui/components/Layout.vue
@@ -10,7 +10,32 @@
 </template>
 
 <script setup>
+import { getUserCenter } from '@/api/index.js';
 import ChatSilder from '../components/ChatSilder.vue';
+import { ElMessage } from 'element-plus';
+import { useRouter } from 'vue-router';
+import { clear } from '../utils/storageHelper';
+import wfc from '@/wfc/client/wfc';
+import IpcEventType from '@/ipcEventType';
+import { ipcRenderer, isElectron } from '@/platform';
+const router = useRouter();
+
+const logout = () => {
+    clear();
+    wfc.disconnect();
+    if (isElectron()) {
+        ipcRenderer.send(IpcEventType.LOGOUT);
+    }
+};
+
+getUserCenter().then((res) => {
+    console.log(9999, res);
+    if (res.code == -9) {
+        ElMessage.error('登录过期，请重新登录');
+        logout();
+        router.push('/');
+    }
+});
 </script>
 
 <style lang="scss" scoped>
